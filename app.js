fc373b1264027652a6253be0e2479832d82ec36c
Tereza Stredova <tereza.stredova@student.sps-cl.cz>
2023-09-23 18:10:03 +0200

    created addTodo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
+function addTodo(){
     if (inputBox.value === ''){
         alert("You must write a task!");        
     }
@@ -9,9 +9,6 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
     }
         inputBox.value = "";
 
@@ -20,9 +17,5 @@ function addTask(){
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        
     }
 },false);
